a0a1aed55cbdaf5cc6b2be2b84180044abbd1786
javidancode <javidanig@code.edu.az>
2022-06-21 03:36:20 +0400

    Added C# method table

---
+++ method/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //SumNumbers(21);
+
+            //SumNumbers(2 ,2);
+
+            //SumNumbers(3, 15);
+
+            //int[] numbers = { 3, 7, 9, 11, 17 };
+
+            //int sum = 0;
+
+            //foreach (var item in numbers)
+            //{
+            //    if (item % 2 == 1)
+            //    {
+            //        sum += item;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            //int[] numbers = { 2, 6, 8, 10, 18 };
+
+            //int sum = 0;
+
+            //foreach (var item in numbers)
+            //{
+            //    if (item % 2 == 0)
+            //    {
+            //        sum += item;
+            //    }
+            //}
+            //Console.WriteLine(sum);
+        }
+
+        // n ededi 3 ve ye bolunur bolunmur
+
+        //      static void SumNumbers( int n = 21 )
+        //{
+        //          //int sum = 0;
+
+        //          //if (n % 3 == 0 && n % 7 == 0) 
+
+        //          //{
+        //          //    Console.WriteLine("divided");
+        //          //}
+        //          //else
+        //          //{
+        //          //    Console.WriteLine(" not divided");
+
+        //          //}
+
+        //      }
+
+        // n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmi
+
+        //      static void SumNumbers(int n = 2 , int m = 2)
+        //      {
+        //         int sum = 0;
+
+        //         for (int i = n; i <= m; i+= 2)
+        //         {
+        //             if (i % 2 == 0) 
+        //             {
+        //                 sum ++;
+        //             }
+        //         }
+
+        //         Console.WriteLine(n+m);
+        //}
+
+
+        // n,m ededleri arasindaki tek ededlerin cemi
+
+        //static void SumNumbers(int n = 3,int m = 15)
+        //{
+        //    int sum = 0;
+
+        //    for (int i = n; i <= m; i++)
+        //    {
+
+        //        if (i%2 == 1)
+        //        {
+        //            sum += i;
+        //        }
+
+        //    }
+
+        //    Console.WriteLine(sum);
+
+        //}
+
+
+        // Verilmish arrayin icindeki tek ededlerin cemi
+
+        //static void Numbers(int[] numbers)
+        //{
+        //    int sum = 0;
+
+        //    foreach (var item in numbers)
+        //    {
+        //        if (item % 2 == 1)
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+
+        //    Console.WriteLine(sum);
+        //}
+
+
+        //Verilmish arrayin icindeki cut ededlerin sayi
+
+  //      static void Numbers(int[] numbers)
+  //{
+  //          int sum = 0;
+
+        //          foreach (var item in numbers)
+        //          {
+        //              if (item %2 == 0)
+        //              {
+        //                  sum += item;
+        //              }
+        //          }
+        //          Console.WriteLine(sum);
+        //      }
+
+
+
+
+    }
+}
